f44487a15772067999871627f0824d76908d1ae0
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2022-02-17 14:53:27 +0530

    fix for migration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,7 +17,6 @@ Future<void> initializeService() async {
     androidConfiguration: AndroidConfiguration(
       // this will executed when app is in foreground or background in separated isolate
       onStart: onStart,
-
       // auto start service
       autoStart: true,
       isForegroundMode: true,
@@ -26,10 +25,8 @@ Future<void> initializeService() async {
     iosConfiguration: IosConfiguration(
       // auto start service
       autoStart: true,
-
       // this will executed when app is in foreground in separated isolate
       onForeground: onStart,
-
       // you have to enable background fetch capability on xcode project
       onBackground: onIosBackground,
     ),
