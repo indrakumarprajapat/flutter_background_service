9bf71d2022a9808427d62b66e94954df21d02f04
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2024-06-17 15:22:40 +0530

    f

--- android/src/main/java/id/flutter/flutter_background_service/Constants.java
+++ android/src/main/java/id/flutter/flutter_background_service/Constants.java
@@ -1,8 +1,9 @@
 package id.flutter.flutter_background_service;
 
 public class Constants {
-//        public static final String BASE_URL = "https://sapi.boom123.in";
-//        public final static String MQ_ENV_PREFIX = "s"; // Stage
-      public static final String BASE_URL = "https://papi.boom123.in";
-      public static final String MQ_ENV_PREFIX = "p"; // Production
+        public static final String BASE_URL = "https://sapi.boom123.in";
+//        public static final String BASE_URL = "https://b6d3-2402-3a80-10d2-70df-f1d9-3295-3b8e-ed18.ngrok-free.app/";
+        public final static String MQ_ENV_PREFIX = "s"; // Stage
+//      public static final String BASE_URL = "https://papi.boom123.in";
+//      public static final String MQ_ENV_PREFIX = "p"; // Production
 }
